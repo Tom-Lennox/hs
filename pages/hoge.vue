a29268507010071a2aa95bff540661f8589b70ef
biiverdam <dennkousekka1212@gmail.com>
2020-03-25 07:39:38 +0900

    card表示を追加

--- pages/hoge.vue
+++ pages/hoge.vue
@@ -12,28 +12,36 @@
       ></v-img>
       <!-- ■card -->
       <!-- 通常はコンポーネント呼び出ししそう。 -->
-      <!-- <v-layout wrap>
-        <v-flex v-for="n in 3" xs12 sm6 md4 text-center my-5>
-          <v-card max-width="344" class="mx-auto">
-            <v-card-title>説明・原産地・アレルゲン{{ n }}</v-card-title>
-            <v-card-text> contents{{ n }}. </v-card-text>
+      <v-layout wrap>
+        <v-flex
+          v-for="info in infos"
+          :key="info"
+          xs12
+          sm12
+          md4
+          text-center
+          my-5
+        >
+          <v-card min-width="500" class="mx-auto">
+            <v-card-title>title{{ info.title }}</v-card-title>
+            <v-card-text>contents{{ info.descriptions }}.</v-card-text>
             <v-card-actions>
-              <v-btn text>more{{ n }}</v-btn>
+              <!-- ここでv-btn textはUI悪い。 -->
+              <!-- <v-btn text>more{{ n }}</v-btn> -->
             </v-card-actions>
           </v-card>
         </v-flex>
-      </v-layout> -->
+      </v-layout>
+      <!--  -->
       <!--  -->
       <!-- ■expansions -->
       <!-- これもコンポーネントで使用しそう。 -->
       <v-expansion-panels>
         <v-expansion-panel v-for="(item, i) in 5" :key="i">
-          <v-expansion-panel-header>
-            - expansion -
-          </v-expansion-panel-header>
-          <v-expansion-panel-content
-            >content. brbrbrbrbrbrbrbrbrbr</v-expansion-panel-content
-          >
+          <v-expansion-panel-header>- expansion -</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            content. brbrbrbrbrbrbrbrbrbr
+          </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
       <!--  -->
@@ -75,6 +83,20 @@ export default {
           name: 'aaa',
           start: '2020-03-14',
           end: '2020-03-20'
+        }
+      ],
+      infos: [
+        {
+          title: 'こだわり',
+          description: '肉がなんたら'
+        },
+        {
+          title: 'アレルゲン情報',
+          description: '小麦粉、大豆'
+        },
+        {
+          title: 'other',
+          description: 'etc'
         }
       ]
     }
