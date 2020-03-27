25533e1a7c860a8a4cb23cea46432095b44efc3b
Tom-Lennox <dennkousekka1212@gmail.com>
2020-03-27 09:24:01 +0900

    top作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,60 @@
 <template>
   <v-layout wrap>
     <!-- <v-layout column justify-center align-center> -->
-    <v-flex class="text-center" xs12 sm12 md12 lg12 xl12 text-center my-5>
-      <v-card max-width="1200" class="mx-auto">
-        <v-card-title class="headline">title</v-card-title>
-        <v-card-text
-          >-------------------------------------------------</v-card-text
-        >
-      </v-card>
-    </v-flex>
-    <v-flex class="text-center">
-      <v-card max-width="1200" class="mx-auto">
-        <v-card-title>title</v-card-title>
+    <!-- ■carousel -->
+    <v-carousel>
+      <!-- cycle：ループ -->
+      <v-carousel-item
+        v-for="(color, i) in colors"
+        :key="color"
+        cycle="true"
+        touch="left"
+      >
+        <v-sheet :color="color" height="100%" tile>
+          <v-row classs="fill-height" align="center" justify="center">
+            <div class="display-3">slide: {{ i + 1 }}</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+    <!--  -->
+    <!-- ■card -->
+    <!-- 画像 -->
+    <v-layout v-for="photo in photos" :key="photo" align-center wrap>
+      <v-flex xs4 sm4 md4 lg4 xl4 text-center mx-5 my-5 wrap
+        ><v-card min-width="150" class="mx-auto ma-2" outlined raised
+          ><v-card-title class="headline">
+            {{ photo.title }}
+          </v-card-title>
+          <v-card-text>
+            <!-- {{ photo.text }} -->
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <!--  -->
+    <!-- ■card -->
+    <v-flex
+      v-for="info in infos"
+      :key="info"
+      class="text-center"
+      xs12
+      sm12
+      md12
+      lg12
+      xl12
+      text-center
+      my-1
+      ><v-card max-width="1200" class="mx-auto"
+        ><v-card-title class="headline">
+          {{ info.title }}
+        </v-card-title>
         <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
+          {{ info.text }}
         </v-card-text>
       </v-card>
     </v-flex>
+    <!--  -->
     <!-- <v-flex xs12 sm8 md6>
       <div class="text-center">
         <logo />
@@ -79,7 +113,7 @@
           </v-btn>
         </v-card-actions>
       </v-card>
-    </v-flex>-->
+    </v-flex> -->
   </v-layout>
 </template>
 
@@ -91,6 +125,36 @@ export default {
   components: {
     // Logo,
     // VuetifyLogo
-  }
+  },
+  data: () => ({
+    colors: ['primary', 'secondary', 'yellow darken-2', 'red', 'orange'],
+    infos: [
+      {
+        title: '重要なお知らせ',
+        text: 'text'
+      },
+      {
+        title: 'お知らせ',
+        text: 'text'
+      }
+    ],
+    photos: [
+      {
+        title: 'http'
+      },
+      {
+        title: 'http'
+      },
+      {
+        title: 'http'
+      },
+      {
+        title: 'http'
+      },
+      {
+        title: 'http'
+      }
+    ]
+  })
 }
 </script>
